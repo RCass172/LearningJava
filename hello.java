c58de0bcab899f603aeb5b3e4278a3930ccfa16b
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-04-15 15:30:08 +0100

    Add new practice statements

--- hello.java
+++
@@ -1,4 +0,0 @@
-// for loop
-for (int i = 0; i < 5; i++){
-    System.out.println("I = "+i);
-}
---
+++ practice.java
@@ -0,0 +1,16 @@
+// declare variables with their data type
+int x = 6;
+double y = 2.65;
+char c = 'C';
+String myString = "Hello";
+
+// To get length of a string
+myString.length(); // 5
+
+// create new object
+Object o = new Object();
+
+// for loop
+for (int i = 0; i < 5; i++){
+    System.out.println("I = "+i);
+}
